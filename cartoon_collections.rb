63573476eae02d24412d69dd8ba5d0dd05c5912a
alpbourne <alpbourne@gmail.com>
2017-09-20 19:54:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  exclaim_upcase: []
+  
 end
 
 def long_planeteer_calls# code an argument here
